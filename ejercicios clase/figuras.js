8c3535ce8bb32a445bff6945c3194f1b5c0aaf20
Alejandra.Moreno <ale.mocr@gmail.com>
2019-05-07 16:35:24 -0500

    carpeta de los ejercicios hechos en clase

---
+++ ejercicios clase/figuras.js
@@ -0,0 +1,30 @@
+module.exports = class Figuras{
+  constructor (tipoFigura, radio, lado1, lado2, altura){
+    this.tipoFigura=tipoFigura;
+    this.radio=radio;
+    this.lado1=lado1;
+    this.lado2=lado2;
+    this.altura=altura;
+  }
+
+
+obtenerArea (){
+    switch (this.tipoFigura.toLowerCase()) {
+      case "circulo":
+          console.log("el area del círculo es "+ (3.14159*(this.radio*this.radio)));
+        break;
+      case "rectangulo":
+          console.log("el area del rectangulo es "+(this.lado1*this.lado2));
+        break;
+      case "cuadrado":
+            console.log("el area del cuadrado es "+(this.lado1*this.lado1));
+        break;
+          case "triangulo":
+              console.log("el area del triangulo es "+((this.lado1*this.altura)/2));
+          break;
+      default:
+        console.log("Figura no soportada");
+        break;
+    }
+  }
+}
---
+++ ejercicios clase/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const Figuras = require('./figuras.js');
+
+let circulo = new Figuras("circulo", 5);
+let cuadrado = new Figuras("cuadrado",null,5);
+let triangulo = new Figuras("triangulo",null,5,null,6);
+let rectangulo = new Figuras("rectangulo",null,7,8);
+circulo.obtenerArea();
+cuadrado.obtenerArea();
+triangulo.obtenerArea();
+rectangulo.obtenerArea();
